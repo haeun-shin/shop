f0675b0208554afc723a72fe4be4c7946aa8e14b
haeun-shin <65892186+haeun-shin@users.noreply.github.com>
2020-12-30 19:55:16 +0900

    Create: VO클래스

---
+++ shop/src/main/java/com/shop/domain/GoodsVO.java
@@ -0,0 +1,76 @@
+package com.shop.domain;
+
+import java.util.Date;
+
+public class GoodsVO {
+/*
+    goodsNum       	number          not null,
+    goodsName      	varchar2(50)    not null,
+    cateCode		varchar2(30)    not null,
+    goodsPrice     	number          not null,
+    goodsStock     	number          null,
+    goodsDesc       varchar(500)    null,
+    goodsImg       	varchar(200)    null,
+    goodsDate      	date            default sysdate,
+    primary key(goodsNum)  
+*/
+	private int goodsNum;
+	private String goodsName;
+	private String cateCode;
+	private int goodsPrice;
+	private int goodsStock;
+	private String goodsDesc;
+	private String goodsImg;
+	private Date goodsDate;
+	
+	public int getGoodsNum() {
+		return goodsNum;
+	}
+	public void setGoodsNum(int goodsNum) {
+		this.goodsNum = goodsNum;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public String getCateCode() {
+		return cateCode;
+	}
+	public void setCateCode(String cateCode) {
+		this.cateCode = cateCode;
+	}
+	public int getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice(int goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	public int getGoodsStock() {
+		return goodsStock;
+	}
+	public void setGoodsStock(int goodsStock) {
+		this.goodsStock = goodsStock;
+	}
+	public String getGoodsDesc() {
+		return goodsDesc;
+	}
+	public void setGoodsDesc(String goodsDesc) {
+		this.goodsDesc = goodsDesc;
+	}
+	public String getGoodsImg() {
+		return goodsImg;
+	}
+	public void setGoodsImg(String goodsImg) {
+		this.goodsImg = goodsImg;
+	}
+	public Date getGoodsDate() {
+		return goodsDate;
+	}
+	public void setGoodsDate(Date goodsDate) {
+		this.goodsDate = goodsDate;
+	}
+	
+	
+}
