203794db5b2a1e8cf713e97bda7814d25fc22123
haeun-shin <65892186+haeun-shin@users.noreply.github.com>
2021-01-07 13:08:43 +0900

    Create: ReplyListVO

---
+++ shop/src/main/java/com/shop/domain/ReplyListVO.java
@@ -0,0 +1,73 @@
+package com.shop.domain;
+
+import java.util.Date;
+
+public class ReplyListVO {
+	/*
+	tbl_reply
+    goodsNum   number not null, (tbl_goods(goodsNum)참조)
+    userId     varchar2(50)   not null, (tbl_member(userId)참조)
+    replyNum   number         not null,
+    replyCon   varchar2(2000) not null,
+    replyDate  date           default sysdate,
+    primary key(goodsNum, replyNum) 
+	*/
+	
+	private int goodsNum;
+	private String userId;
+	private int replyNum;
+	private String replyCon;
+	private Date replyDate;
+	
+	private String userName;
+
+	public int getGoodsNum() {
+		return goodsNum;
+	}
+
+	public void setGoodsNum(int goodsNum) {
+		this.goodsNum = goodsNum;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getReplyNum() {
+		return replyNum;
+	}
+
+	public void setReplyNum(int replyNum) {
+		this.replyNum = replyNum;
+	}
+
+	public String getReplyCon() {
+		return replyCon;
+	}
+
+	public void setReplyCon(String replyCon) {
+		this.replyCon = replyCon;
+	}
+
+	public Date getReplyDate() {
+		return replyDate;
+	}
+
+	public void setReplyDate(Date replyDate) {
+		this.replyDate = replyDate;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+}
