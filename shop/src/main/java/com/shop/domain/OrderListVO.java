aa31866ed740e1297d8b1ba2d9ac109232677965
haeun-shin <65892186+haeun-shin@users.noreply.github.com>
2021-01-11 13:02:13 +0900

    Create: OrderListVO

---
+++ shop/src/main/java/com/shop/domain/OrderListVO.java
@@ -0,0 +1,116 @@
+package com.shop.domain;
+
+import java.util.Date;
+
+public class OrderListVO {
+	private String orderId;
+	private String userId;
+	private String orderRec;
+	private String userAddr1;
+	private String userAddr2;
+	private String userAddr3;
+	private String orderPhone;
+	private int amount;
+	private Date orderDate;
+	
+	private int orderDetailsNum;
+	private int goodsNum;
+	private int cartStock;
+	
+	private String goodsName;
+	private String goodsThumbImg;
+	private int goodsPrice;
+	
+	public String getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getOrderRec() {
+		return orderRec;
+	}
+	public void setOrderRec(String orderRec) {
+		this.orderRec = orderRec;
+	}
+	public String getUserAddr1() {
+		return userAddr1;
+	}
+	public void setUserAddr1(String userAddr1) {
+		this.userAddr1 = userAddr1;
+	}
+	public String getUserAddr2() {
+		return userAddr2;
+	}
+	public void setUserAddr2(String userAddr2) {
+		this.userAddr2 = userAddr2;
+	}
+	public String getUserAddr3() {
+		return userAddr3;
+	}
+	public void setUserAddr3(String userAddr3) {
+		this.userAddr3 = userAddr3;
+	}
+	public String getOrderPhone() {
+		return orderPhone;
+	}
+	public void setOrderPhone(String orderPhone) {
+		this.orderPhone = orderPhone;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public int getOrderDetailsNum() {
+		return orderDetailsNum;
+	}
+	public void setOrderDetailsNum(int orderDetailsNum) {
+		this.orderDetailsNum = orderDetailsNum;
+	}
+	public int getGoodsNum() {
+		return goodsNum;
+	}
+	public void setGoodsNum(int goodsNum) {
+		this.goodsNum = goodsNum;
+	}
+	public int getCartStock() {
+		return cartStock;
+	}
+	public void setCartStock(int cartStock) {
+		this.cartStock = cartStock;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public String getGoodsThumbImg() {
+		return goodsThumbImg;
+	}
+	public void setGoodsThumbImg(String goodsThumbImg) {
+		this.goodsThumbImg = goodsThumbImg;
+	}
+	public int getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice(int goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	
+	
+}
